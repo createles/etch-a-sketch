a866b5fc8fcd3a5d912fbac1601321d3e42803d8
createls <justinecastillo7@gmail.com>
2024-12-02 16:00:51 +0900

    Fix grid squares logic to populate row and column wise.
    Add condition check to customize grid size button.
    
    Set lower alpha for grid square borders.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,42 @@
 let mainContainer = document.querySelector("#mainContainer");
 
-function populateGrid() {
-    for (let i = 0; i < 16; i++) {
-        let gridSquare = document.createElement("div");
-        gridSquare.className = "gridSquare";
-        mainContainer.appendChild(gridSquare);
+function populateGrid(size) {
+    for (let i = 0; i < size; i++) {
+        let gridRowContainer = document.createElement("div");
+        gridRowContainer.className = "gridRowContainer";
+        mainContainer.appendChild(gridRowContainer);
+        for (let j = 0; j < size; j++) {
+            let gridSquare = document.createElement("div");
+            gridSquare.className = "gridSquare";
+            gridSquare.addEventListener("mouseover", () => gridSquare.style.backgroundColor = "blue");
+            gridRowContainer.appendChild(gridSquare);
+        }
     }
 }
 
-populateGrid();
+
+
+function resize(newSize) {
+    mainContainer.innerHTML = "";
+    populateGrid(newSize);
+}
+
+let resizeButton = document.querySelector("#gridSize");
+resizeButton.addEventListener("click", () => {
+    let size;   
+    do {
+        size = prompt("Please input your desired canvas pixel-size (maximum of 100).");
+        size = Number(size);
+
+        if (size > 100) {
+            alert("Cannot generate more than 100 grid tiles per side. Please enter a number less than 100.");
+        } else if (size < 2 || !Number.isInteger(Number(size))) {
+            alert("Must input a whole number from 2 to 100.");
+        }   
+
+    } while (!(size <= 100 && size >= 2 && Number.isInteger(Number(size))))
+
+    resize(size);
+}); 
+
+populateGrid(16);
