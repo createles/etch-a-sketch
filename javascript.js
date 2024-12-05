b8664d1706eb63c796b25c6065bf74c0624d5de0
createlsworklappy <justinecastillo7@gmail.com>
2024-12-05 15:53:06 +0900

    Add color changing switches
    Added monotone and multicolor options.
    
    Code first time load behavior
    
    Set base color to monotone.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 let mainContainer = document.querySelector("#mainContainer");
+let size = 16;
 
 function populateGrid(size) {
     for (let i = 0; i < size; i++) {
@@ -8,12 +9,33 @@ function populateGrid(size) {
         for (let j = 0; j < size; j++) {
             let gridSquare = document.createElement("div");
             gridSquare.className = "gridSquare";
-            gridSquare.addEventListener("mouseover", () => gridSquare.style.backgroundColor = "blue");
+            gridSquare.addEventListener("mouseover", () => gridSquare.style.backgroundColor = `rgb(0,0,0`);
+            /* !CODE TO ADD OPACITY CHANGES!
+             gridSquare.addEventListener("mouseover", () => {
+                let currentOpacity = Number(gridSquare.style.opacity);
+                gridSquare.style.opacity = Math.min(currentOpacity + 0.1, 1);
+            }); */
             gridRowContainer.appendChild(gridSquare);
         }
     }
 }
 
+
+let multicolor = document.querySelector("#multicolor");
+multicolor.addEventListener("click", () => {
+    resize(size);
+    document.querySelectorAll(".gridSquare").forEach(element => {
+        element.addEventListener("mouseover", () => element.style.backgroundColor = `rgb(${randomizeRGB()},${randomizeRGB()},${randomizeRGB()})`)
+    });
+})
+
+let monotone = document.querySelector("#monotone");
+monotone.addEventListener("click", () => {
+    resize(size);
+    document.querySelectorAll(".gridSquare").forEach(element => {
+        element.addEventListener("mouseover", () => element.style.backgroundColor = `rgb(0,0,0`)
+    });
+})
 
 
 function resize(newSize) {
@@ -21,9 +43,13 @@ function resize(newSize) {
     populateGrid(newSize);
 }
 
+function randomizeRGB() {
+    let num = Math.floor(Math.random()*256);
+    return num;
+}
+
 let resizeButton = document.querySelector("#gridSize");
-resizeButton.addEventListener("click", () => {
-    let size;   
+resizeButton.addEventListener("click", () => {  
     do {
         size = prompt("Please input your desired canvas pixel-size (maximum of 100).");
         size = Number(size);
